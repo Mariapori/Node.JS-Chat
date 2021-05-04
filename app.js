be8fa5d2c7b56ec65444da04beafaa768ccbfbc4
Topias Mariapori <topias@mariapori.fi>
2021-05-04 10:04:54 +0300

    Korjauksia

--- app.js
+++ app.js
@@ -9,25 +9,35 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+var history = [];
 
 io.on('connection', (socket) => {
+
+    GetHistory(socket);
     console.log('Joku liittyi osoitteesta ' + socket.request.socket.remoteAddress);
+
     socket.on('disconnect', () => {
         console.log('Joku poistui osoitteesta ' + socket.request.socket.remoteAddress);
     });
+
     socket.on('chat message', (msg, nick) => {
       if(socket.nick == null){
         socket.nick = nick;
         io.to(socket.id).emit('nickname set');
       }
-        var now = new Date().toLocaleDateString("fi-FI");
         var nowtime = new Date().toLocaleTimeString("fi-FI")
         console.log(msg + ' osoitteesta ' + socket.request.socket.remoteAddress);
         io.emit('chat message', nowtime + ' ' + msg);
+        history.push(nowtime + ' ' + msg);
       });
+
   });
   
 
 server.listen(5665,'0.0.0.0', () => {
   console.log('Kuunnellaan *:5665');
 });
+
+function GetHistory(socket){
+  io.to(socket.id).emit('history', history);
+}
