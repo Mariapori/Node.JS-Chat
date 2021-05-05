7bde2cd0ee5ec0eb4e2fa0e90f0a06e95673d187
Topias Mariapori <topias@mariapori.fi>
2021-05-05 13:45:21 +0300

    Kaikenlaista mukavaa online users ja welc/exit msg

--- app.js
+++ app.js
@@ -11,23 +11,42 @@ app.get('/', (req, res) => {
 
 var history = [];
 
+var users = [];
+
 io.on('connection', (socket) => {
 
+    GetUsername(socket);
     GetHistory(socket);
-    console.log('Joku liittyi osoitteesta ' + socket.request.socket.remoteAddress);
 
     socket.on('disconnect', () => {
-        console.log('Joku poistui osoitteesta ' + socket.request.socket.remoteAddress);
+        io.emit('user left', socket.nick + ' poistui.');
+        console.log(socket.nick + ' poistui');
+        for (let index = 0; index < users.length; index++) {
+          if(users[index].id == socket.id){
+            users.splice(index, 1);
+          }
+        }
     });
+    
+     socket.on('set user', (nick) => {
+        socket.nick = nick;
+        users.push({ id : socket.id, nick : nick});
+        console.log(nick + ' liittyi');
+        io.emit('user welcome', nick + ' liittyi.');
+     });
+
+     socket.on('GetUsers', () => {
+      io.emit('OnlineUsers', users);
+   });
+
 
     socket.on('chat message', (msg, nick) => {
       if(socket.nick == null){
         socket.nick = nick;
-        io.to(socket.id).emit('nickname set');
       }
         var now = new Date().toLocaleDateString("fi-FI");
         var nowtime = new Date().toLocaleTimeString("fi-FI");
-        console.log(msg + ' osoitteesta ' + socket.request.socket.remoteAddress);
+        console.log(msg);
         io.emit('chat message', nowtime + ' ' + msg);
         history.push(now + ' | ' + nowtime + ' ' + msg);
       });
@@ -41,4 +60,7 @@ server.listen(5665, () => {
 
 function GetHistory(socket){
   io.to(socket.id).emit('history', history);
+}
+function GetUsername(socket){
+  io.to(socket.id).emit('user connected');
 }
